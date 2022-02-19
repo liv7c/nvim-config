dc4695b34fedc8267aabe5fb4daad71faee3e43e
Olivia Coumans <livie7c@gmail.com>
2022-02-19 10:01:18 +0100

    feat(packer): set up packer with basic plugins

---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,36 @@
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+packer.startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" 
+
+  -- must have
+  use 'tpope/vim-surround'
+  use "numToStr/Comment.nvim" -- for commenting
+
+  -- colorscheme
+  use 'EdenEast/nightfox.nvim'
+end)
