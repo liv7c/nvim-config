afa6c8a634a09cd0e058a552d0140a959ac6f93a
Olivia Coumans <livie7c@gmail.com>
2022-02-19 10:10:58 +0100

    feat(mappings): add custom keymaps

---
+++ nvim/lua/user/mappings.lua
@@ -0,0 +1,59 @@
+local opts = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+--------------
+-- Normal --
+--------------
+
+-- back and forth navigation
+keymap("n", ",,", "<C-^>", opts)
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- copy the rest of the line from the location you are in the line
+keymap("n", "Y", "y$", opts)
+
+-- make file executable
+keymap("n", "<leader>x", ":!chmod +x %<CR>", opts)
+
+-------------
+-- Visual --
+-------------
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- nvim tree
+keymap("n", "<leader>n", "<cmd>NvimTreeToggle<cr>", opts)
+keymap("n", "<leader>r", "<cmd>NvimTreeRefresh<cr>", opts)
