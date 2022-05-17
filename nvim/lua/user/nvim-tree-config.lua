acd94dd03bd88e4c982de5cbd2c5c0a193309ca8
Olivia Coumans <livie7c@gmail.com>
2022-05-17 20:07:14 +0200

    feat(nvim-tree): remove unused property

--- nvim/lua/user/nvim-tree-config.lua
+++ nvim/lua/user/nvim-tree-config.lua
@@ -9,7 +9,6 @@ nvim_tree.setup {
   hijack_netrw         = true,
   open_on_setup        = false,
   ignore_ft_on_setup   = {},
-  auto_close           = false,
   auto_reload_on_write = true,
   open_on_tab          = false,
   hijack_cursor        = false,
