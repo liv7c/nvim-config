3e348c72f37c9614ae2b862abf679c841db40654
Olivia Coumans <livie7c@gmail.com>
2024-01-27 16:57:51 +0100

    chore: migrate from packer to lazy vim

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
 # My neovim config
 
-Neovim config using LSP (work in progress). All config files are written in Lua.
+Neovim config using LSP. All config files are written in Lua.
+It uses `lazy.nvim` as a plugin manager.
 
 ## Requirements
 
+There are a couple of dependencies necessary for this configuration to work.
+
 - Install [`fd`](https://github.com/sharkdp/fd) and [`ripgrep`](https://github.com/BurntSushi/ripgrep). Those two packages are important for the Telescope plugin to work.
-- This config has been tested with a version of `neovim` that is **at least** `0.9.1`
-- You should also have `packer.nvim` installed. Check out [the installation instructions](https://github.com/wbthomason/packer.nvim#quickstart).
+- This config has been tested with a version of `neovim` that is **at least** `0.9.2`. If you haven't installed or have an older version of neovim, check out the [neovim installation documentation](https://github.com/neovim/neovim/blob/master/INSTALL.md).
 
 ## Plugins used
 
 Some of the plugins used:
 
-- [packer](https://github.com/wbthomason/packer.nvim): plugin manager
+- [lazy.nvim](https://github.com/folke/lazy.nvim): it is a plugin manager specifically developed for neovim ❤️
 - [nvim-lspconfig](https://github.com/neovim/nvim-lspconfig)
 - [mason](https://github.com/williamboman/mason.nvim): to install and manage LSP language servers and formatters
 - [null-ls](https://github.com/jose-elias-alvarez/null-ls.nvim): for formatting and linting
 - [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter): for syntax highlighting
 - [telescope](https://github.com/nvim-telescope/telescope.nvim): for fuzzy finding
-- [nvim-tree](https://github.com/kyazdani42/nvim-tree.lua): file explorer
+- [nvim-tree](https://github.com/kyazdani42/nvim-tree.lua): a simple file explorer
 - [vim-surround](https://github.com/tpope/vim-surround)
 - [vim-fugitive](https://github.com/tpope/vim-fugitive)
 - [nvim-autopairs](https://github.com/windwp/nvim-autopairs)
 
 ## Further resources
 
-If you feel like building your own config, here are some resources that I found really helpful:
+If you feel like building your own config, here are some resources that I have found really helpful:
 
 - [Neovim from scratch by chris@machine](https://www.youtube.com/playlist?list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ): great playlist to get started configuring neovim using lua.
 - [Neovim from scratch repository](https://github.com/LunarVim/Neovim-from-scratch)
--- nvim/lua/olivia/plugins.lua
+++ nvim/lua/olivia/plugins.lua
@@ -1,121 +1,88 @@
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
-
-local packer_bootstrap = ensure_packer()
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
-
-local status, packer = pcall(require, "packer")
-if not status then
-	return
-end
-
-return packer.startup(function(use)
-	use("wbthomason/packer.nvim")
-
-	use({
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	-- colorscheme
+	{
 		"rose-pine/neovim",
 		as = "rose-pine",
 		styles = { italic = false },
-	})
-
-	-- use({
-	-- 	"EdenEast/nightfox.nvim",
-	-- 	config = function()
-	-- 		require("nightfox").setup({
-	-- 			options = {
-	-- 				styles = {
-	-- 					comments = "italic",
-	-- 				},
-	-- 				colorblind = {
-	-- 					enable = true,
-	-- 				},
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- })
-
+	},
 	-- essential plugins
-	use("tpope/vim-surround")
-	use("tpope/vim-commentary")
-	use("windwp/nvim-autopairs")
-	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
-
-	-- tree plugin
-	use({
+	"tpope/vim-surround",
+	"tpope/vim-commentary",
+	"windwp/nvim-autopairs",
+	{ "windwp/nvim-ts-autotag", after = "nvim-treesitter" },
+	{
+			"nvim-tree/nvim-tree.lua",
+			requires = {
+				"nvim-tree/nvim-web-devicons", -- optional, for file icons
+			},
+	},
+	-- tree + cooler status line
+	{
 		"nvim-tree/nvim-tree.lua",
 		requires = {
 			"nvim-tree/nvim-web-devicons", -- optional, for file icons
 		},
-	})
-
-	-- status line
-	use({
-		"nvim-lualine/lualine.nvim",
+	},
+	{"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+	},
 
 	-- fuzzy finder
-	use("nvim-lua/plenary.nvim")
-	use("nvim-telescope/telescope.nvim")
+	"nvim-lua/plenary.nvim",
+	"nvim-telescope/telescope.nvim",
 
 	-- easier navigation
-	use("ThePrimeagen/harpoon")
+	"ThePrimeagen/harpoon",
 
 	-- autocompletion
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
 
 	-- snippets
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
-	use("rafamadriz/friendly-snippets")
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
 
 	-- lsp
-	-- for mananing and installing lsp servers and formatters
-	use({ "williamboman/mason.nvim" })
-	use({ "williamboman/mason-lspconfig.nvim" })
+	-- for managing and installing lsp servers and formatters
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
 	-- for configuring lsp servers
-	use({ "neovim/nvim-lspconfig" })
-	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	{ "neovim/nvim-lspconfig" },
+	"hrsh7th/cmp-nvim-lsp", -- for autocompletio,
+	"jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename,file & update imports)
+	"onsails/lspkind.nvim", -- vs-code like icons for autocompletio,
 
 	-- for formatting and linting
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("jayp0521/mason-null-ls.nvim")
+	"jose-elias-alvarez/null-ls.nvim",
+	"jayp0521/mason-null-ls.nvim",
 
 	-- easier error navigation
-	use("folke/trouble.nvim")
+	"folke/trouble.nvim",
 
 	-- treesitter
-	use({
+	{
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
 			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
 		end,
-	})
+	},
 
 	-- git
-	use("tpope/vim-fugitive")
-
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
+	"tpope/vim-fugitive",
+})
