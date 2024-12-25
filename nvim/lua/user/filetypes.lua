605af8d52b0b91237852a9b5539368888210dfe5
Olivia Coumans <livie7c@gmail.com>
2024-12-25 07:29:36 +0100

    feat: add custom indentation rules

---
+++ nvim/lua/user/filetypes.lua
@@ -0,0 +1,12 @@
+local group = vim.api.nvim_create_augroup('CustomIndentation', { clear = true })
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = group,
+  pattern = { 'go', 'python', 'rust' },
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = true -- Ensure spaces are used instead of tabs
+  end,
+})
