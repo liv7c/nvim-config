a172016955a98dc4fa378c24552813d214fa1663
Olivia Coumans <livie7c@gmail.com>
2024-12-25 07:39:09 +0100

    feat(format): add formatting for html and lua

--- nvim/lua/user/plugins/conform.lua
+++ nvim/lua/user/plugins/conform.lua
@@ -8,6 +8,8 @@ return {
       javascript = {"prettierd", "prettier", "eslint_d", stop_after_first = true },
       typescript = { "prettierd", "prettier", "eslint_d", stop_after_first = true },
       typescriptreact = { "prettierd", "prettier", "eslint_d", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
+      lua = { "stylua" },
     },
     format_on_save = {
       timeout_ms = 1000,
