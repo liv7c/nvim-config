cdbd214e0131a9b65e3777dd10695543531520aa
liv7c <livie7c@gmail.com>
2025-05-05 12:09:16 +0200

    chore(cmp): remove copilot from completion

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -8,7 +8,6 @@ return {
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    -- 'hrsh7th/cmp-copilot',
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind-nvim",
@@ -27,7 +26,6 @@ return {
       luasnip = "LuaSnip",
       nvim_lua = "Lua",
       path = "Path",
-      copilot = "Copilot",
     }
 
     local function ltrim(s)
@@ -123,7 +121,6 @@ return {
         { name = "nvim_lsp" },
         { name = "nvim_lsp_signature_help" },
         { name = "luasnip" },
-        -- { name = 'copilot' },
         { name = "buffer" },
         { name = "path" },
       },
