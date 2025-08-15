9409a49c93f8ca99a5947da9b2d05ec27d4f80d9
Olivia Coumans <livie7c@gmail.com>
2025-08-15 13:04:32 +0200

    feat(nvim-tree): increase width and change location

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -3,9 +3,18 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional, for file icons
     config = function()
+      local nvim_tree = require("nvim-tree")
+
       vim.keymap.set("n", "<Leader>n", "<cmd>NvimTreeToggle<cr>")
       vim.keymap.set("n", "<Leader>nf", "<cmd>NvimTreeFindFileToggle<cr>")
-      require("nvim-tree").setup({})
+      vim.keymap.set("n", "<Leader>nr", "<cmd>NvimTreeRefresh<cr>")
+
+      nvim_tree.setup({
+        view = {
+          width = 40,
+          side = "right",
+        },
+      })
     end,
   },
 }
