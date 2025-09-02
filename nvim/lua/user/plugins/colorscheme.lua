d1304530a5bfa1f5005c2f4f6297cde1a54230fb
Olivia Coumans <livie7c@gmail.com>
2025-09-02 07:04:57 +0200

    feat(colorscheme): update colorscheme

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -1,10 +1,28 @@
 return {
-  "tinted-theming/tinted-vim",
+  "AlexvZyl/nordic.nvim",
+  -- "tinted-theming/tinted-vim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.tinted_bold = 0
-    vim.cmd("colorscheme base16-gruvbox-dark-hard")
-    vim.api.nvim_set_hl(0, "Comment", { fg = "#928374" })
+    require("nordic").setup({
+      on_highlight = function(highlights)
+        -- Make Visual selection clearly visible
+        highlights.Visual = {
+          bg = "#3c4043",
+          fg = nil,
+        }
+        highlights.VisualNOS = {
+          bg = "#3c4043",
+        }
+        highlights.Comment = {
+          fg = "#7C8489",
+          italic = true,
+        }
+      end,
+    })
+    require("nordic").load()
+    -- vim.g.tinted_bold = 0
+    -- vim.cmd("colorscheme base16-gruvbox-dark-hard")
+    -- vim.api.nvim_set_hl(0, "Comment", { fg = "#928374" })
   end,
 }
