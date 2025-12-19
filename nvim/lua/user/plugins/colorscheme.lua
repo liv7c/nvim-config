0d4c426c531f01d748a0cb4bbdcef50e31c8bb72
Olivia Coumans <livie7c@gmail.com>
2025-12-19 09:07:44 +0100

    chore: update deps and colorscheme file structure

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -9,31 +9,36 @@
 --         vim.cmd.colorscheme('sonokai')
 --       end
 -- }
+-- return {
+--   "jonathanfilip/vim-lucius",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme("lucius")
+--     -- Match the gutter background with the editor background
+--     vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+--     vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+--     vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "NONE" })
+--     vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "NONE" })
+--     -- Set indent-blankline highlight groups
+--     vim.api.nvim_set_hl(0, "IblScope", { fg = "#5f87af" })
+--   end,
+-- }
+
 return {
-  "AlexvZyl/nordic.nvim",
-  -- "tinted-theming/tinted-vim",
+  "tinted-theming/tinted-vim",
   lazy = false,
-  priority = 1000,
   config = function()
-    require("nordic").setup({
-      on_highlight = function(highlights)
-        -- Make Visual selection clearly visible
-        highlights.Visual = {
-          bg = "#3c4043",
-          fg = nil,
-        }
-        highlights.VisualNOS = {
-          bg = "#3c4043",
-        }
-        highlights.Comment = {
-          fg = "#7C8489",
-          italic = true,
-        }
-      end,
-    })
-    require("nordic").load()
-    -- vim.g.tinted_bold = 0
-    -- vim.cmd("colorscheme base16-gruvbox-dark-hard")
-    -- vim.api.nvim_set_hl(0, "Comment", { fg = "#928374" })
+    vim.g.tinted_bold = 0
+    vim.cmd("colorscheme base16-gruvbox-dark-hard")
+    vim.api.nvim_set_hl(0, "Comment", { fg = "#928374" })
   end,
 }
+
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   config = function()
+--     vim.cmd("colorscheme rose-pine-moon")
+--   end,
+-- }
